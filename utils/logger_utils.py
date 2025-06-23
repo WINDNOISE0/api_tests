b47fbd6e69f55d54e670a80c2410d71097a44906
Jaroslav T <jarileslava@gmail.com>
2025-06-23 13:25:11 +0200

    fix 3

--- utils/logger_utils.py
+++ utils/logger_utils.py
@@ -11,17 +11,17 @@ class LoggerUtils:
     def log_response(func):
         def _log_response(*args, **kwargs) -> requests.Response:
             response = func(*args, **kwargs)
-            Logger.info(f"=====================    Request    =====================")
+            Logger.info("=====================    Request    =====================")
             Logger.info(f"ENDPOINT: {response.url}")
             Logger.info(f"{curlify.to_curl(response.request)}")
             Logger.info(f"Headers request: \n{response.headers}")
             body = json.dumps(response.json(), indent=2) if JsonUtils.is_json(response.text) else response.text
             Logger.info("")
-            Logger.info(f"=====================    Response    =====================")
+            Logger.info("=====================    Response    =====================")
             Logger.info(f"Status code: '{response.status_code}', elapsed_time='{response.elapsed}']")
-            Logger.info(f"---------------------------body---------------------------")
+            Logger.info("---------------------------body---------------------------")
             Logger.info(f"{body}")
-            Logger.info(f"----------------------------------------------------------\n\n\n")
+            Logger.info("----------------------------------------------------------\n\n\n")
             return response
 
         return _log_response
